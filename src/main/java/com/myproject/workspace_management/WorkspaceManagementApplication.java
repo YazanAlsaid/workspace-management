fbddfbc5a6ccfc87862ab1d178c05be459a39edb
Yazan Alsaid <yazan7.jad@icloud.com>
2024-12-23 12:41:39 +0100

    ++++++++

---
+++ src/main/java/com/myproject/workspace_management/WorkspaceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.workspace_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkspaceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkspaceManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myproject/workspace_management/WorkspaceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.workspace_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkspaceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
